ffff4f083c16721f1dd8b5734d142e506ee813c7
Vincent <vincentjian@potix.com>
2015-07-27 18:18:36 +0800

    fine tune display message for authenticated vs remembered

--- src/main/java/shiro/sample/HomeComposer.java
+++ src/main/java/shiro/sample/HomeComposer.java
@@ -4,6 +4,7 @@
 package shiro.sample;
 
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Wire;
@@ -22,10 +23,14 @@ public class HomeComposer extends SelectorComposer<Component> {
 
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
-		if (SecurityUtils.getSubject().isRemembered()) {
-			user.setValue("Welcome Back: " + SecurityUtils.getSubject().getPrincipal() + ", you need to login again to access protected page.");
+		Subject subject = SecurityUtils.getSubject();
+		if (subject.isAuthenticated()) {
+			user.setValue("Welcome: you are currently logged in as " + subject.getPrincipal());
+		} else if (subject.isRemembered()) {
+			user.setValue("Welcome Back: " + subject.getPrincipal()
+					+ ", you need to login again to access protected page.");
 		} else {
-			user.setValue("");
+			user.setValue("Unknown user: you'll have to log in");
 		}
 	}
 }
